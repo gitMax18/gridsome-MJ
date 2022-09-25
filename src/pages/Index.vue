321768a9e9861ead1af04fd05408fd86cfea2e07
LAPTOP-ML0416D6\68790 <je.maxime@hotmail.com>
2022-09-26 08:31:54 +1100

    startup project

---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+    <Layout>
+        <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+        <g-image alt="Example image" src="~/assets/default.jpg" width="135" />
+
+        <h1>Hello World !</h1>
+
+        <p>
+            Bonjour je m'appelle Maxime JEAN je suis apprenti développeur web.
+        </p>
+
+        <p class="home-links">
+            <a href="https://gridsome.org/docs/" target="_blank">Gridsome Docs</a>
+            <a href="https://github.com/gridsome/gridsome" target="_blank">GitHub</a>
+        </p>
+    </Layout>
+</template>
+
+<script>
+export default {
+    metaInfo: {
+        title: "Hello, world!",
+    },
+};
+</script>
+
+<style>
+.home-links a {
+    margin-right: 1rem;
+}
+</style>
